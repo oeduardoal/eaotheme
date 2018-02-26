6fd2ea46474527a8478a9758bcc8c55c78027acc
Eduardo Almeida <eduardoalmeida258@gmail.com>
2018-02-25 22:04:34 -0300

    Starting

---
+++ functions.php
@@ -0,0 +1,81 @@
+<?php
+	
+	ini_set('memory_limit', '2048M');
+
+
+	// No FTP
+	define('FS_METHOD', 'direct');
+
+	# Require
+	####################################################
+	// Mail
+	require_once "mail/mail.php";
+	// Configs
+	foreach(glob(get_template_directory() . "/configs/*.php") as $filename)
+    require_once $filename;
+
+	####################################################
+	
+	// CONSTANTES
+	DEFINE('assetsurl', get_template_directory_uri());
+	DEFINE('thumbnail_default', get_template_directory_uri() . "/assets/img/padrao-thumbnail.jpg");
+
+	####################################################
+	
+	function load_assets() {
+	
+		// My LIB
+		// wp_enqueue_script( 'app_js',  assetsurl . '/assets/js/m/jq.min.js');
+		// wp_enqueue_style( 'app_css', assetsurl . '/assets/css/main.min.css');
+
+	}
+	add_action( 'wp_enqueue_scripts', 'load_assets' );
+
+	####################################################
+
+	function register_my_menus() {
+		register_nav_menus(
+			array(
+			  'sub-menu' => __( 'Sub Menu' )
+			)
+		);
+		register_nav_menus(
+			array(
+			  'header-menu' => __( 'Header Menu' )
+			)
+		);
+	}
+	add_action( 'init', 'register_my_menus' );
+
+	####################################################
+	
+	// Registro das suas widgets
+	if ( function_exists('register_sidebar') ){
+	    register_sidebar(array(
+	        'name' => __( 'Widget 1'),
+	        'id' => 'widget-1',
+	        'description' => __( ''),
+	        'before_title' => '<h1>',
+	        'after_title' => '</h1>',
+	    ) );
+	}
+
+	####################################################
+	function get_menu_obj( $location ) {
+	    if( empty($location) ) return false;
+
+	    $locations = get_nav_menu_locations();
+	    if( ! isset( $locations[$location] ) ) return false;
+
+	    $menu_obj = get_term( $locations[$location], 'nav_menu' );
+
+	    return $menu_obj;
+	}
+	####################################################
+	
+	// Tamanho da largura das imagens.
+	if ( function_exists( 'add_theme_support' ) ) {
+		add_theme_support( 'post-thumbnails' );
+		set_post_thumbnail_size( 480, 350, true );
+		
+	}
---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head <?php language_attributes(); ?>>
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	<![endif]-->
+  	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="icon" href="http://oeduardoal.github.io/eduardoalmeida.ico">
+	<?php wp_head(); ?>
+	<meta name="theme-color" content="#000">
+</head>
+<body <?php body_class(); ?>>
