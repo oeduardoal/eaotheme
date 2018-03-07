ffd47cb7431cc1597fff7b722f147d1fa3ebeabf
Eduardo Almeida <eduardoalmeida258@gmail.com>
2018-03-07 00:30:58 -0300

    build

--- header.php
+++ header.php
@@ -10,6 +10,16 @@
 	<link rel="icon" href="http://oeduardoal.github.io/eduardoalmeida.ico">
 	<?php wp_head(); ?>
 	<meta name="theme-color" content="#000">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-115318800-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-115318800-1');
+</script>
+
 </head>
 <body <?php body_class(); ?>>
 
