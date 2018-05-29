b463ad5721b3d78c7d7839d9a0a028ff3e74294a
Eduardo Almeida <oeduardoal@gmail.com>
2018-05-28 23:11:29 -0300

    DCF Video

--- includes/video-tpl.php
+++ includes/video-tpl.php
@@ -1,5 +1,5 @@
 <section id="video" class="container-fluid">
     <div class="row justify-content-lg-center">
-        <iframe src="https://www.youtube.com/embed/COQOSSKOCjU?rel=0&  amp;controls=0&amp;showinfo=0" frameborder="0"allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/dnTs8noNtZE?rel=0&  amp;controls=0&amp;showinfo=0" frameborder="0"allow="autoplay; encrypted-media" allowfullscreen></iframe>
     </div>
 </section>
